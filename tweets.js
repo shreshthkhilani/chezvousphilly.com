c39a8cea9fd434e24d8f0f82e874b36d45c49191
Shreshth Khilani <shreshth.khilani@gmail.com>
2018-09-06 08:53:22 -0400

    Create tweet cards and make it easy to add tweets

---
+++ tweets.js
@@ -0,0 +1,13 @@
+tweets = [
+	{
+		"img": "/01.jpg",
+		"title": "\"We'll get to you\"",
+		"text": "Serving you in your kitchen anywhere in Greater Philadelphia. <a target=\"_blank\" href=\"https://goo.gl/forms/LGSrAvIPGMINPPfj1\">Reserve Now!</a>",
+		"date": "6:02 PM September 5, 2018"
+	},
+	{
+		"img": "/chezvous.png",
+		"text": "les chefs sont arrivés... chez vous",
+		"date": "June 1, 2018"
+	}
+];
